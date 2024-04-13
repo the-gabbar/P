981910d0cb2d1c60b3e5664391e34e4fa4f49631
MAI GABBAR <141297334+the-gabbar@users.noreply.github.com>
2024-04-13 17:58:51 +0530

    Update save_file.py

--- pyrogram/methods/advanced/save_file.py
+++ pyrogram/methods/advanced/save_file.py
@@ -1,21 +1,3 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
-#
-#  This file is part of Pyrogram.
-#
-#  Pyrogram is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published
-#  by the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Pyrogram is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
 import asyncio
 import functools
 import inspect
@@ -44,56 +26,6 @@ class SaveFile:
         progress: Callable = None,
         progress_args: tuple = ()
     ):
-        """Upload a file onto Telegram servers, without actually sending the message to anyone.
-        Useful whenever an InputFile type is required.
-
-        .. note::
-
-            This is a utility method intended to be used **only** when working with raw
-            :obj:`functions <pyrogram.api.functions>` (i.e: a Telegram API method you wish to use which is not
-            available yet in the Client class as an easy-to-use method).
-
-        .. include:: /_includes/usable-by/users-bots.rst
-
-        Parameters:
-            path (``str`` | ``BinaryIO``):
-                The path of the file you want to upload that exists on your local machine or a binary file-like object
-                with its attribute ".name" set for in-memory uploads.
-
-            file_id (``int``, *optional*):
-                In case a file part expired, pass the file_id and the file_part to retry uploading that specific chunk.
-
-            file_part (``int``, *optional*):
-                In case a file part expired, pass the file_id and the file_part to retry uploading that specific chunk.
-
-            progress (``Callable``, *optional*):
-                Pass a callback function to view the file transmission progress.
-                The function must take *(current, total)* as positional arguments (look at Other Parameters below for a
-                detailed description) and will be called back each time a new file chunk has been successfully
-                transmitted.
-
-            progress_args (``tuple``, *optional*):
-                Extra custom arguments for the progress callback function.
-                You can pass anything you need to be available in the progress callback scope; for example, a Message
-                object or a Client instance in order to edit the message with the updated progress status.
-
-        Other Parameters:
-            current (``int``):
-                The amount of bytes transmitted so far.
-
-            total (``int``):
-                The total size of the file.
-
-            *args (``tuple``, *optional*):
-                Extra custom arguments as defined in the ``progress_args`` parameter.
-                You can either keep ``*args`` or add every single extra argument in your function signature.
-
-        Returns:
-            ``InputFile``: On success, the uploaded file is returned in form of an InputFile object.
-
-        Raises:
-            RPCError: In case of a Telegram RPC error.
-        """
         async with self.save_file_semaphore:
             if path is None:
                 return None
@@ -127,11 +59,6 @@ class SaveFile:
 
             if file_size == 0:
                 raise ValueError("File size equals to 0 B")
-
-            file_size_limit_mib = 12000 if self.me.is_premium else 6000
-
-            if file_size > 6 * 1024 * 1024 * 1024:
-                raise ValueError(f"Can't upload files bigger than {file_size_limit_mib} MiB")
 
             file_total_parts = int(math.ceil(file_size / part_size))
             is_big = file_size > 10 * 1024 * 1024
